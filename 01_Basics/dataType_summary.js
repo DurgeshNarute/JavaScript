1603013d56a6f36e2495555f879c5f40487ec821
DurgeshNarute <durgeshnarute@gmail.com>
2023-12-19 23:25:02 +0530

    Data type summary is added

---
+++ 01_Basics/dataType_summary.js
@@ -0,0 +1,14 @@
+/*
+Depending on how data is stored and accessed, datatypes are majorly categorised in 2 types:
+1.Primitive: call by value
+  > 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+  > copy is created of the data
+
+  
+2.Non-Primitive/ reference type
+  > Array, Objects, Functions
+
+JavaScript is a dynamically typed language, ie no need to mention the datatype of variables
+ 
+
+ */
