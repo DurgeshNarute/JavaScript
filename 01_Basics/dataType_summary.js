c455e7eb63a9da46334eaa2cb2c9e5af9a2d47f1
DurgeshNarute <durgeshnarute@gmail.com>
2024-01-13 17:40:17 +0530

    Added Details

--- 01_Basics/dataType_summary.js
+++ 01_Basics/dataType_summary.js
@@ -3,10 +3,12 @@ Depending on how data is stored and accessed, datatypes are majorly categorised 
 1.Primitive: call by value
   > 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
   > copy is created of the data
+  > Stack Memory
 
   
 2.Non-Primitive/ reference type
   > Array, Objects, Functions
+  > Heap Memory
 
 JavaScript is a dynamically typed language, ie no need to mention the datatype of variables
  
